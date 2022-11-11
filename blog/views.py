488634bdd28fd8737338a4c4bf593b8a1907fcbb
Kollecollier <kristoffer.collier@live.se>
2022-11-11 11:54:01 +0000

    Added first view and edited some css

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, Views
 from .models import Post
 
 
 class Postlist(generic.ListView):
-    model = post
+    model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 8
