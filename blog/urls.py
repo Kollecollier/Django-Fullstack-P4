76fe6a2477b7c142089e91953db87984fd293660
Kollecollier <kristoffer.collier@live.se>
2022-11-11 12:34:50 +0000

    added post detail view and get definition

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.Postlist.as_view(), name='home')
+    path('', views.Postlist.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
