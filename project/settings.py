203435387dfe3a7cbb5f2776c418fba5639b4442
Kollecollier <kristoffer.collier@live.se>
2022-12-01 10:24:51 +0000

    set cloudinary url

--- project/settings.py
+++ project/settings.py
@@ -144,6 +144,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+# Cloudinary Url
+
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+}
+
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
