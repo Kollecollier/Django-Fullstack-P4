b6f4b98c7b2cfc40a290ab74b0fe633f01525308
Kollecollier <kristoffer.collier@live.se>
2023-05-12 16:23:02 +0000

    updated datase url

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-1337.herokuapp.com', 'localhost']
 
